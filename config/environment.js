6d1f1b16b14428ed885d55821310046679adcb00
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-08-14 23:34:36 +0100

    Updated environment variables

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,10 @@
+require('dotenv').config()
+
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost/han-solo${process.env.NODE_ENV || 'dev'}`
-const secret = process.env.SECRET || 'hansolo'
+
+// the NODE_ENV suffix to the database URL allow us to have 
+// multiple MongoDB databases, depending on the node environment.
+const dbURI = process.env.MONGODB_URI || process.env.MONGODB_URL + `-${process.env.NODE_ENV || 'dev'}`
+const secret = process.env.SECRET || 'local-test'
 
 module.exports = { port, dbURI, secret }
